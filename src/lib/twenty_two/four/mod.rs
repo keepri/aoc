c89134f888cad4ec9d1ac37257ae3854bad89921
KIPRI <hi@kipri.dev>
2023-08-11 16:32:01 +0300

    2022 day 4 small naming changes

--- src/lib/twenty_two/four/mod.rs
+++ src/lib/twenty_two/four/mod.rs
@@ -7,17 +7,15 @@ pub fn run_four() -> Result<()> {
     let mut part_one_score: i32 = 0;
     let mut part_two_score: i32 = 0;
 
-    while let Some(section_id_range_pair) = input.next() {
-        let section_id_range_pair = section_id_range_pair.trim();
+    while let Some(range_pair) = input.next() {
+        let range_pair = range_pair.trim();
 
-        if let Some((range_one, range_two)) = section_id_range_pair.split_once(",") {
+        if let Some((range_one, range_two)) = range_pair.split_once(",") {
             if let (Some((start_one, end_one)), Some((start_two, end_two))) =
                 (range_one.split_once("-"), range_two.split_once("-"))
             {
-                let start_one = start_one.parse::<i32>()?;
-                let end_one = end_one.parse::<i32>()?;
-                let start_two = start_two.parse::<i32>()?;
-                let end_two = end_two.parse::<i32>()?;
+                let (start_one, end_one) = (start_one.parse::<i32>()?, end_one.parse::<i32>()?);
+                let (start_two, end_two) = (start_two.parse::<i32>()?, end_two.parse::<i32>()?);
 
                 if !(start_one < start_two || end_one > end_two)
                     || !(start_two < start_one || end_two > end_one)
