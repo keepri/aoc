9b2e0312a02d8cca1ba35adea54e99878a27e2a4
KIPRI <hi@kipri.dev>
2024-12-07 23:04:06 +0200

    2024 day 3 part 1

--- 2024/cmd/aoc/three.go
+++ 2024/cmd/aoc/three.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/keepri/adventofcode/internal/data"
+)
+
+const (
+	MUL         = "mul"
+	PARAN_OPEN  = '('
+	PARAN_CLOSE = ")"
+	COMMA       = ","
 )
 
 type dayThree struct{}
@@ -14,15 +23,58 @@ func (d dayThree) solve(part int) (int, error) {
 		return 0, err
 	}
 
-	safe := 0
+	out := 0
 	for _, line := range lines {
 		switch part {
+		// TODO recurse
 		case 1:
-			fmt.Println(line)
+			split := strings.SplitAfter(line, MUL)
+		chunk:
+			for _, chunk := range split {
+				if len(chunk) < 5 {
+					continue
+				}
+				if chunk[0] != PARAN_OPEN {
+					continue
+				}
+				params := strings.Split(chunk, COMMA)
+				if len(params) < 2 {
+					continue
+				}
+				firstParam := -1
+				for i, param := range params {
+					if len(param) < 2 {
+						continue chunk
+					}
+					if i == 2 {
+						continue chunk
+					}
+					if i == 0 {
+						firstParam, err = strconv.Atoi(param[1:])
+						if err != nil {
+							continue chunk
+						}
+					} else {
+						index := strings.Index(param, ")")
+						if index == -1 {
+							continue chunk
+						}
+						secondParam, err := strconv.Atoi(param[:index])
+						if err != nil {
+							continue chunk
+						}
+						out += firstParam * secondParam
+					}
+				}
+			}
 		default:
 			return 0, fmt.Errorf("Invalid part: %v used for day 1", part)
 		}
 	}
 
-	return safe, nil
+	return out, nil
+}
+
+func (d dayThree) partOne(line string) (int, error) {
+	return 0, nil
 }
